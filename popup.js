8b3551efb1a1fe2199cfb4fad14aabe211cddf36
dsernst <git@dsernst.com>
2014-11-14 04:11:39 -0600

    (cleanup) improve readability and version num

--- popup.js
+++ popup.js
@@ -4,26 +4,7 @@
 // Connect with other who are feeling what you're feeling at that exact moment.
 // by: letsfix.net
 
-
-var gatherTabInfo = function(){
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
-    var url = tabs[0].url;
-    var title = tabs[0].title;
-
-    passInfoToPopup(url, title);
-  });
-};
-
-
-var passInfoToPopup = function(url, title){
-  $('#pageURL').html(url);
-//  $('#pageURL').attr('href', url);    // commented out due to focus bug.
-  $('#pageTitle').html(title);
-  $('#commentField').focus();
-};
-
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   $('#submitComment').click(function (e) {
     e.preventDefault();
@@ -31,8 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-
-var collectComment = function(){
+var collectComment = function () {
   var url = $('#pageURL').html();
   var title = $('#pageTitle').html();
 
@@ -43,61 +23,57 @@ var collectComment = function(){
   var comment = $('#commentField').val();
 
   buildMessage(url, title, comment);
+};
 
-}
-
-
-var encodeWeirdChars = function(string){
+var encodeWeirdChars = function (string) {
   var encoded = encodeURI(string);
-  var normalSpaces = encoded.replace(/%20/g," ");
+  var normalSpaces = encoded.replace(/%20/g, ' ');
   return normalSpaces;
 };
 
-
-var buildMessage = function(url, title, comment){
+var buildMessage = function (url, title, comment) {
   // Build email parameters (to, subject, body)
-  var message = "To: <problems@letsfix.net>\nSubject: [Lfx-post] " + title + "\n\n" + url + "\n\n" + comment;
+  var message = {'title': title, 'url': url, 'note': comment};
 
   console.log(message);
 
-  auth(message);
-
+  postItem(message);
 };
 
-
-var auth = function(message){
-  // Ask user permission to authorize Gmail OAuth API
-  chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-    
-    console.log(token);
-
-    sendMessage(message, noteSuccess() );
+var postItem = function (message) {
+  var timestamp = new Date().getTime();
+  var uploadPath = 'http://lfxpost.s3.amazonaws.com/receive/' + timestamp + '.json';
+  $.ajax({
+    type: "PUT",
+    url: uploadPath,
+    dataType: 'json',
+    async: false,
+    data: JSON.stringify(message),
+    error: noteErrors,
+    success: noteSuccess
   });
-
 };
 
-
-// Send mail through SMTP with Gmail OAuth API
-// see https://developers.google.com/gmail/api/v1/reference/users/messages/send
-var sendMessage = function(message, callback){
-  var base64EncodedEmail = btoa(message);
-  var request = gapi.client.gmail.users.messages.send({
-    // 'me' is a special value, uses authenticated user
-    'userId': 'me',
-    'message': {
-      'raw': base64EncodedEmail
-    }
-  });
-  request.execute(callback);
+var noteErrors = function (jqXHR, textStatus, errorThrown) {
+  console.log(jqXHR + '\n\n' + textStatus + '\n\n' + errorThrown);
 };
 
-
-var noteSuccess = function(){
+var noteSuccess = function (data, textStatus, jqXHR) {
   document.body.innerHTML = "<h3>Success...?</h3>";
+  console.log(data + '\n\n' + textStatus + '\n\n' + jqXHR);
 };
-
 
 // Begin running our script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  gatherTabInfo();
+  // Gather Tab Info
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    var title = tabs[0].title;
+
+    // Pass Info To Popup
+    $('#pageURL').html(url);
+    //  $('#pageURL').attr('href', url);    // commented out due to focus bug.
+    $('#pageTitle').html(title);
+    $('#commentField').focus();
+  });
 });
