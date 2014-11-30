d79ac66e1dc77cdbf6d44941906f5f2ba3843882
dsernst <git@dsernst.com>
2014-11-29 22:10:45 -0500

    cleanup: simplify popup.js, remove old gAPI permissions

--- popup.js
+++ popup.js
@@ -4,40 +4,19 @@
 // Connect with other who are feeling what you're feeling at that exact moment.
 // by: letsfix.net
 
-document.addEventListener('DOMContentLoaded', function () {
-
-  $('#submitComment').click(function (e) {
-    e.preventDefault();
-    collectComment();
-  });
-});
-
-var collectComment = function () {
-  var url = $('#pageURL').html();
-  var title = $('#pageTitle').html();
-
-  // escape special characters to prep for base64 encoding
-  url = encodeWeirdChars(url);
-  title = encodeWeirdChars(title);
-
-  var comment = $('#commentField').val();
-
-  buildMessage(url, title, comment);
-};
-
 var encodeWeirdChars = function (string) {
   var encoded = encodeURI(string);
   var normalSpaces = encoded.replace(/%20/g, ' ');
   return normalSpaces;
 };
 
-var buildMessage = function (url, title, comment) {
-  // Build email parameters (to, subject, body)
-  var message = {'title': title, 'url': url, 'note': comment};
+var noteErrors = function (jqXHR, textStatus, errorThrown) {
+  console.error(jqXHR + '\n\n' + textStatus + '\n\n' + errorThrown);
+};
 
-  console.log(message);
-
-  postItem(message);
+var noteSuccess = function (data, textStatus, jqXHR) {
+  document.body.innerHTML = "<h3>Success</h3>";
+  console.info(data + '\n\n' + textStatus + '\n\n' + jqXHR);
 };
 
 var postItem = function (message) {
@@ -54,26 +33,27 @@ var postItem = function (message) {
   });
 };
 
-var noteErrors = function (jqXHR, textStatus, errorThrown) {
-  console.log(jqXHR + '\n\n' + textStatus + '\n\n' + errorThrown);
-};
-
-var noteSuccess = function (data, textStatus, jqXHR) {
-  document.body.innerHTML = "<h3>Success...?</h3>";
-  console.log(data + '\n\n' + textStatus + '\n\n' + jqXHR);
-};
-
 // Begin running our script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
+  var message = {};
+
   // Gather Tab Info
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    var title = tabs[0].title;
+    message.url = encodeWeirdChars(tabs[0].url);
+    message.title = encodeWeirdChars(tabs[0].title);
 
     // Pass Info To Popup
-    $('#pageURL').html(url);
+    $('#pageURL').html(message.url);
     //  $('#pageURL').attr('href', url);    // commented out due to focus bug.
-    $('#pageTitle').html(title);
+    $('#pageTitle').html(message.title);
     $('#commentField').focus();
+  });
+
+  $('#submitComment').click(function (e) {
+    e.preventDefault();
+    message.comment = $('#commentField').val();
+    message.timestamp = new Date().getTime();
+    console.log(message);
+    postItem(message);
   });
 });
