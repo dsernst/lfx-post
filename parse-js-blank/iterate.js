3f689c5de74942506aa9d5f991b31a3b74310347
Omar Duarte <omarduarte@gmail.com>
2015-01-08 22:01:06 -0800

    Fixed dates and timestamps in index

--- parse-js-blank/iterate.js
+++ parse-js-blank/iterate.js
@@ -3,7 +3,7 @@ var iterate = function (array) {
  var results = [];
  var $tableEntry;
  array.forEach(function (entry) {
-   $tableEntry = "<tr><th scope='row'>" + counter + "</th><td>" + entry.user + "</td><td>" + entry.content + "</td><td>" + entry.url + "</td><td>" + entry.date + "</td><td>" + entry.votes + "</td></tr>";  
+   $tableEntry = "<tr><th scope='row'>" + counter + "</th><td>" + entry.user + "</td><td>" + entry.comment + "</td><td>" + entry.url + "</td><td>" + new Date(entry.timestamp) + "</td><td>" + entry.votes + "</td></tr>";  
    counter++;
    results.push($tableEntry);
  });
